678f4bf7bef5b89aec230be06d61a1453b38e2b1
Somogyi Benedek <somogyi.benedek0525@gmail.com>
2024-03-24 23:46:54 +0100

    control switch-case done

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,14 +2,21 @@ package src.game;
 
 import src.character.Student;
 
+import java.io.BufferedReader;
 import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static src.game.SingletonLogger.logger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
 
-        ConsoleApp.writeOutInventory(new Student(null, 0));
+        String number = "";
+        while(number != "q"){
+            ConsoleApp.writeOutInventory(new Student(null, 0));
         logger.info("BugBusters ProjLab tesztprogram");
         logger.info("Tesztesetek:");
         logger.info("1.  Logarlec");
@@ -29,25 +36,33 @@ public class Main {
         logger.info("15. Jatek kezdete");
         logger.info("16. Osszetett eset");
         logger.info("17. Targyeldobasa");
+        logger.info("q: Exit");
         logger.warning("Ejnye, bebugzott valami! (mock warning)");
-
+        
+        number = reader.readLine();
         Tester tester = new Tester();
-        tester.test1();
-        tester.test2();
-        tester.test3();
-        tester.test4();
-        tester.test5();
-        tester.test6();
-        tester.test7();
-        tester.test8();
-        tester.test9();
-        tester.test10();
-        tester.test11();
-        tester.test12();
-        tester.test13();
-        tester.test14();
-        tester.test15();
-        tester.test16();
-        tester.test17();
+        switch(number){
+            case "1": tester.test1(); break;
+            case "2": tester.test2(); break;
+            case "3": tester.test3(); break;
+            case "4": tester.test4(); break;
+            case "5": tester.test5(); break;
+            case "6": tester.test6(); break;
+            case "7": tester.test7(); break;
+            case "8": tester.test8(); break;
+            case "9": tester.test9(); break;
+            case "10": tester.test10(); break;
+            case "11": tester.test11(); break;
+            case "12": tester.test12(); break;
+            case "13": tester.test13(); break;
+            case "14": tester.test14(); break;
+            case "15": tester.test15(); break;
+            case "16": tester.test16(); break;
+            case "17": tester.test17(); break;
+            default: break;
+        }
+        System.out.println();
+        }
+        
     }
 }
