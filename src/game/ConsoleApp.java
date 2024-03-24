d9e89fac992ee590b02d7181bb857ac8ca867b8f
Balogh István Barnabás <bdbarni@gmail.com>
2024-03-24 23:57:02 +0100

    Fix ConsoleApp v2.0

--- src/game/ConsoleApp.java
+++ src/game/ConsoleApp.java
@@ -2,49 +2,28 @@ package src.game;
 
 import src.character.Character;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 import static src.game.SingletonLogger.logger;
 
 public class ConsoleApp {
-    private static HashMap<Object, Integer> calls;
+    private static int tabCounter = 0;
 
-    static  {
-        calls = new HashMap<>();
-    }
-
-    public static void consoleLog(Object caller, Object called, String text){
-        //A gyoker hozzaadasa
-        if(calls.isEmpty())
-            calls.put(caller, 0);
-
-        //Ha visszalep a faban torli a gyerekeit
-        ArrayList<Object> forDelete = new ArrayList<>();
-        for(Map.Entry<Object, Integer> call : calls.entrySet()){
-            if(call.getValue() > calls.get(caller))
-                forDelete.add(call);
-        }
-        //Itt torli
-        for(Object o : forDelete)
-            calls.remove(o);
-
-        //A fuggveny tabulatorozasnal a hivo+1 lesz
-        int tabNum = calls.get(caller) + 1;
-
-        //Ha nincs benne hozzaadja az adot tabulator szammal egyutt
-        if(!calls.containsKey(called))
-            calls.put(called, tabNum);
-
-        //Kiiras
-        for(int i = 0; i < tabNum; i++)
+    public static void funcLog(String func){
+        for(int i = 0;i < tabCounter; i++){
             System.out.print('\t');
-        System.out.println(text);
+        }
+        System.out.println(func);
+        tabCounter++;
     }
 
-    public static void reset() { calls.clear(); }
+    public static void returnLog(String returnValue){
+        for(int i = 0;i < tabCounter-1; i++){
+            System.out.print('\t');
+        }
+        System.out.println(returnValue);
+        tabCounter--;
+    }
 
     public static boolean writeOutInventory(Character character) {
         logger.info("Current player's id: " + character.getId());
