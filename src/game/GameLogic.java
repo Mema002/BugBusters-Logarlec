827cfc58697d4f8ff774194165a57dd70c33f2d9
Mucsi Attila Gábor <mucsiatigabor@gmail.com>
2024-03-24 20:36:51 +0100

    generate Rooms,characters,items

--- src/game/GameLogic.java
+++ src/game/GameLogic.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import src.character.Character;
+import src.character.Student;
+import src.character.Teacher;
+import src.item.Batskin;
+import src.item.Item;
+import src.item.Sliderule;
 import src.room.Room;
 import src.room.RoomManager;
 import static src.game.SingletonLogger.logger;
@@ -71,12 +76,47 @@ public class GameLogic{
 
     }
 
-    public static void generateCharacters(int count) {
-
+    public static void generateCharacters(int studentCount, int teacherCount) {
+        for (int i = 0; i < studentCount; i++) {
+            characters.add(new Student(null, i)); //null az most ideiglenes, lehet inkább ki kéne venni a konstruktorból
+        }
+        for (int i = 0; i < teacherCount; i++) {
+            characters.add(new Teacher(null, i /*+ studentCount*/));
+        }
     }
 
     public static void generateItems(int count) {
-
+        ArrayList<Item> items = new ArrayList<Item>();
+        ArrayList<Room> rooms = roomManager.getRooms();
+        int roomCount = rooms.size();
+        items.add(new Sliderule(null));
+        for (int i = 0; i < count - 1; i++) {
+            int type = random.nextInt(6);
+            switch (type) {
+                case 0:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                case 1:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                case 2:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                case 3:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                case 4:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                case 5:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                case 6:
+                    rooms.get(random.nextInt(roomCount)).addItem(new Batskin(null));
+                    break;
+                default: break;
+            }
+        }
     }
 
     public static ArrayList<Character> getStudents() { //wtf
