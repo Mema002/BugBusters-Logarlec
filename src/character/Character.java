1472e334ef18a1d70e37ce419074a449a5f1a1c7
Balogh István Barnabás <bdbarni@gmail.com>
2024-03-25 10:40:37 +0100

    Character logged

--- src/character/Character.java
+++ src/character/Character.java
@@ -23,44 +23,38 @@ public abstract class Character {
     public abstract int getId();
 
     public void addToInventory(Item i) {
-        ConsoleApp.funcLog("Character.addToInventory(Item: i)");
         ConsoleApp.returnLog("return");
         inventory.add(i);
     }
 
     public void removeItem(Item i) {
-        ConsoleApp.funcLog("Character.removeInventory(Item: i)");
         ConsoleApp.returnLog("return");
         inventory.remove(i);
     }
 
     public ArrayList<Item> getInventory() {
-        ConsoleApp.funcLog("Character.getInventory()");
         ConsoleApp.returnLog("return inventory");
         return inventory;
     }
 
     public Room getCurrentRoom() {
-        ConsoleApp.funcLog("Character.getCurrentRoom()");
         ConsoleApp.returnLog("return currentRoom");
         return currentRoom;
     }
 
     public int getStunnedFor() {
-        ConsoleApp.funcLog("Character.getStunnedFor()");
         ConsoleApp.returnLog("return stunnedFor");
         return stunnedFor;
     }
 
     public void beStunnedFor(int i) {
-        ConsoleApp.funcLog("Character.beStunnedFor(int: i)");
         ConsoleApp.returnLog("return");
         stunnedFor = i;
     }
 
     public void dropItem(Item i) {
-        ConsoleApp.funcLog("Character.dropItem()");
         inventory.remove(i);
+        ConsoleApp.funcLog("currentRoom.addItem(i)");
         currentRoom.addItem(i);
         ConsoleApp.funcLog("i.dropItem()");
         i.drop();
@@ -68,7 +62,6 @@ public abstract class Character {
     }
 
     public void dropItems() {
-        ConsoleApp.funcLog("Character.dropItems()");
         for (Item i : inventory) {
             ConsoleApp.funcLog("currentRoom.addItem(Item: i)");
             currentRoom.addItem(i);
@@ -80,7 +73,7 @@ public abstract class Character {
     public abstract void move(int targetIndex);
 
     public void pickUpItem(int targetIndex) {
-        ConsoleApp.funcLog("Character.pickUpItems(int: targetIdx)");
+        ConsoleApp.funcLog("currentRoom.getItems()");
         ArrayList<Item> options = currentRoom.getItems();
         if (options.isEmpty()){
             ConsoleApp.returnLog("return");
@@ -88,33 +81,32 @@ public abstract class Character {
         }
         Item chosen = options.get(targetIndex);
         if(inventory.size()<5){
+            ConsoleApp.funcLog("this.addToInventory(chosen)");
             addToInventory(chosen);
+            ConsoleApp.funcLog("currentRoom.removeItem(chosen)");
             currentRoom.removeItem(chosen);
+            ConsoleApp.funcLog("chosen.initItem(this)");
             chosen.initItem(this);
         }
         ConsoleApp.returnLog("return");
     }
 
     public void skipTurn() {
-        ConsoleApp.funcLog("Character.skipTurn()");
         ConsoleApp.returnLog("return");
         return; //sztem csak ennyi (?)
     }
 
     public boolean setExpelled() {
-        ConsoleApp.funcLog("Character.setExpelled()");
         ConsoleApp.returnLog("return false");
         return false;
     }
 
     public void setRoom(Room r) {
-        ConsoleApp.funcLog("Character.setRoom(Room: r)");
         ConsoleApp.returnLog("return");
         currentRoom = r;
     }
 
-    public void useItem(int idx) { 
-        ConsoleApp.funcLog("Character.useItem(int: idx)");
+    public void useItem(int idx) {
         ConsoleApp.returnLog("return");
         return; };
 
@@ -123,22 +115,20 @@ public abstract class Character {
     public abstract boolean triggerExpelling(Student s);
 
     public boolean tryStun() {
-        ConsoleApp.funcLog("Character.tryStun()");
         ConsoleApp.returnLog("return true");
         return true; };
 
     public abstract boolean tryExpell(Teacher attacker);
 
     public void endOfRound() {
-        ConsoleApp.funcLog("Character.endOfRound()");
         for (Item item : inventory) {
+            ConsoleApp.funcLog("item.decrRemainingTime()");
             item.decrRemainingTime();
         }
         ConsoleApp.returnLog("return");
     }
 
-    public void clearInventory() { 
-        ConsoleApp.funcLog("Character.clearInventory()");
+    public void clearInventory() {
         ConsoleApp.returnLog("return");
         inventory.clear(); }
 
