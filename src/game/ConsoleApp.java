1fb4b3e981fc5a30db7b8ba5401dd9cbd63c0380
Balogh István Barnabás <bdbarni@gmail.com>
2024-04-07 10:54:04 +0200

    stateLog fix 1.0

--- src/game/ConsoleApp.java
+++ src/game/ConsoleApp.java
@@ -10,6 +10,7 @@ import java.util.*;
 import static src.game.SingletonLogger.logger;
 
 public class ConsoleApp {
+    private static boolean writeFuncCall = true;
     private static int tabCounter;
 
     private static String consoleBuffer;
@@ -23,32 +24,34 @@ public class ConsoleApp {
     }
 
     public static void funcLog(String func){
-        for(int i = 0;i < tabCounter; i++){
-            System.out.print('\t');
+        if(writeFuncCall){
+            for(int i = 0;i < tabCounter; i++){
+                System.out.print('\t');
+            }
+            System.out.println(func);
+            tabCounter++;
         }
-        System.out.println(func);
-        tabCounter++;
     }
 
     public static void returnLog(String returnValue){
-        for(int i = 0;i < tabCounter-1; i++){
-            System.out.print('\t');
+        if(writeFuncCall){
+            for(int i = 0;i < tabCounter-1; i++){
+                System.out.print('\t');
+            }
+            System.out.println(returnValue);
+            if (tabCounter > 0)
+                tabCounter--;
         }
-        System.out.println(returnValue);
-        if (tabCounter > 0)
-            tabCounter--;
     }
 
     public static void addRoom(Room room){rooms.add(room);}
 
     public static void stateLog() {
-
-        System.out.println("\nstart stateLog\n");
+        toggleFuncCall();
+        System.out.println("\n\nSTART STATELOG\n");
 
         for(int i = 0; i<rooms.size(); i++){
-
             Room room = rooms.get(i);
-            //System.out.println(room.toString()+""+i);
             consoleBuffer+=room.toString();
             consoleBuffer+=i+1;
             consoleBuffer+='\n';
@@ -56,13 +59,11 @@ public class ConsoleApp {
             //Room items
             ConsoleApp.funcLog("room.getItems()");
             ArrayList<Item> itemList = room.getItems();
-            //System.out.println("\tRoom items:");
             consoleBuffer+="\tRoom items:\n";
             //Item count for index
             ArrayList<String> itemCount = new ArrayList<>();
             for(int k = 0; k < itemList.size(); k++){
                 Item item = itemList.get(k);
-                //System.out.println("\t\t"+ item.toString + "" + itemCount.stream().filter(x -> x == item.toString()).count()+1);
                 consoleBuffer+="\t\t";
                 consoleBuffer+=item.toString();
                 consoleBuffer+=itemCount.stream().filter(x -> x == item.toString()).count()+1;
@@ -73,12 +74,10 @@ public class ConsoleApp {
             //Characters
             ConsoleApp.funcLog("room.getCharacters()");
             ArrayList<Character> characterList = room.getCharacters();
-            //System.out.println("\tCharacters:");
             consoleBuffer+="\tCharacters\n";
             ArrayList<String> characterCount = new ArrayList<>();
             for (int l = 0; l < characterList.size(); l++){
                 Character character = characterList.get(l);
-                //System.out.println("\t\t"+ character.toString() + "" + characterCount.stream().filter(x->x == character.toString()).count()+1);
                 consoleBuffer+="\t\t";
                 consoleBuffer+=character.toString();
                 consoleBuffer+=characterCount.stream().filter(x->x == character.toString()).count()+1;
@@ -88,17 +87,15 @@ public class ConsoleApp {
                 //Character items
                 ConsoleApp.funcLog("character.getInventory()");
                 itemList = character.getInventory();
-                //System.out.println("\t\tCharacter items:");
                 consoleBuffer+="\t\tCharacter items:\n";
                 //Item count for index
                 itemCount = new ArrayList<>();
                 for(int k = 0; k < itemList.size(); k++){
                     Item item = itemList.get(k);
-                    //System.out.println("\t\t\t"+item.toString() + "" + itemCount.stream().filter(x -> x == item.toString()).count()+1);
                     consoleBuffer+="\t\t\t";
                     consoleBuffer+=item.toString();
                     consoleBuffer+=itemCount.stream().filter(x -> x == item.toString()).count()+1;
-                    consoleBuffer+='\n';
+                    consoleBuffer+="\n";
                     itemCount.add(item.toString());
                 }
             }
@@ -106,29 +103,32 @@ public class ConsoleApp {
             //Effects
             ConsoleApp.funcLog("room.getEffects()");
             ArrayList<Effect> effectList = room.getEffects();
-            //System.out.println("\tRoom effects: ");
             consoleBuffer+="\tRoom effects:\n";
             for(Effect effect : effectList){
-                //System.out.println("\t\t"+effect.toString());
                 consoleBuffer+="\t\t";
                 consoleBuffer+=effect.toString();
                 consoleBuffer+='\n';
             }
 
         }
-        System.out.print("\n\n");
         System.out.print(consoleBuffer);
         consoleBuffer = new String();
 
-        System.out.println("\nend stateLog\n");
+        System.out.println("\nEND STATELOG\n");
+        toggleFuncCall();
     }
 
     public static void resetState() {
+        writeFuncCall = true;
         rooms.clear();
     }
 
     public static boolean writeOutInventory(Character character) {
         logger.info("Current player's id: " + character.getId());
         return true;
+    }
+
+    public static void toggleFuncCall(){
+        writeFuncCall = !writeFuncCall;
     }
 }
