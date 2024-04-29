2976a1d8f591f74ce01e586be4bda0a2daec637b
Mészáros Márk <107580664+Mema002@users.noreply.github.com>
2024-04-29 11:54:17 +0200

    Add Airfreshener durability

--- src/item/Airfreshener.java
+++ src/item/Airfreshener.java
@@ -4,7 +4,11 @@ import src.character.Character;
 import src.effect.Effect;
 
 public class Airfreshener extends Item {
+    private int durability;
 
+    public Airfreshener(){
+        this.durability=1;
+    }
     /** 
      * Airfreshener hasznalata, kiszelloztet egy gazos szobat
      * @param c
@@ -12,11 +16,14 @@ public class Airfreshener extends Item {
      */
     @Override
     public boolean useItem(Character c) {
-        for (Effect e : c.getCurrentRoom().getEffects())
-            e.clearGas(c.getCurrentRoom());
-
-        owner.removeItem(this);
-        return true;
+        if(durability > 0){
+            durability--;
+            for (Effect e : c.getCurrentRoom().getEffects())
+                e.clearGas(c.getCurrentRoom());
+    
+            owner.removeItem(this);
+            return true;
+        }
     }
 
     @Override
